11d137e0d570c4096e304ac8c1b2f61ec41fc81d
lil-mast <christiantazma77@gmail.com>
2025-06-25 17:17:44 -0700

    Corrected commit for explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,11 +2,11 @@ from datetime import datetime, timedelta
 
 def main():
     current_time = datetime.now()
-    print("Current date and time:", current_time.strftime("%Y-%m-%d %H:%M:%S"))
+    print("Current date and time:", current_time.strftime("%y-%m-%d %H:%M:%S"))
 
     days_to_add = int(input("Enter number of days to add: "))
     future_date = current_time + timedelta(days=days_to_add)
-    print("Future date:", future_date.strftime("%Y-%m-%d"))
+    print("Future date:", future_date.strftime("%y-%m-%d"))
 
 if __name__ == "__main__":
     main()
